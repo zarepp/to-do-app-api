4681e618f1ec403d53f980fd7a19ac9d0581f663
Zharif Othman <zharifothman92@gmail.com>
2018-10-03 16:16:12 +0800

    add get post put delete method

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,4 @@
+// const Joi = require('joi');
 const functions = require('firebase-functions');
 const express = require('express');
 
@@ -36,7 +37,8 @@ app.get('/api/courses/:id', (req, res) => {
 app.post('/api/courses', (req, res) => {
   if (!req.body.name || req.body.name.length < 3) {
     // 400 Bad Request
-    res.status(400).send('Name is required and should be minimum 33 character');
+    res.status(400).send('Name is required and should be minimum 3 character');
+    return;
   }
 
   const course = {
@@ -44,6 +46,48 @@ app.post('/api/courses', (req, res) => {
     name: req.body.name
   };
   courses.push(course);
+  res.send(course);
+});
+
+app.put('/api/courses/:id', (req, res) => {
+  if (!req.body.name || req.body.name.length < 3) {
+    // 400 Bad Request
+    res.status(400).send('Name is required and should be minimum 3 character');
+    return;
+  }
+
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  course.name = req.body.name;
+  res.send(course);
+});
+
+app.put('/api/courses/:id', (req, res) => {
+  if (!req.body.name || req.body.name.length < 3) {
+    // 400 Bad Request
+    res.status(400).send('Name is required and should be minimum 3 character');
+    return;
+  }
+
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send('The course with the give ID not found');
+
+  course.name = req.body.name;
+  res.send(course);
+});
+
+app.delete('/api/courses/:id', (req, res) => {
+  if (!req.body.name || req.body.name.length < 3) {
+    // 400 Bad Request
+    res.status(400).send('Name is required and should be minimum 3 character');
+    return;
+  }
+
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send('The course with the give ID not found');
+
+  const idx = courses.indexOf(course);
+  courses.splice(idx, 1);
+
   res.send(course);
 });
 
